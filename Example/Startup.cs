9e87aa502fc14421dd88cbd8a1232e059303e97d
tpluscode <tpluscode@users.noreply.github.com>
2018-12-06 19:57:58 +0100

    .net standard

---
+++ Example/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Example;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Nancy.Owin;
+
+namespace Example
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOwin(b => b.UseNancy(options => options.Bootstrapper = new Bootstrapper()));
+        }
+    }
+}
